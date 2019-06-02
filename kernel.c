8c77ad6c18345bbb0de4ae431d1eb17fb2160508
Ruben Ollesch <ruben.ollesch@pm.me>
2019-06-02 02:24:35 +0200

    Remove color parameter from putentryat

--- kernel.c
+++ kernel.c
@@ -72,10 +72,10 @@ void terminal_initialize(void)
 	}
 }
 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
+void terminal_putentryat(char c, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
+	terminal_buffer[index] = vga_entry(c, terminal_color);
 }
 
 void terminal_putchar(char c)
@@ -86,7 +86,7 @@ void terminal_putchar(char c)
 		return;
 	}
 
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	terminal_putentryat(c, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
