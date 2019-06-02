e3eca0b482896ed9a04923fd44da8c28cf8135e8
Ruben Ollesch <ruben.ollesch@pm.me>
2019-06-02 02:55:42 +0200

    Fix terminal wrap on line break

--- kernel.c
+++ kernel.c
@@ -76,15 +76,17 @@ void terminal_putchar(char c)
 	if (c == '\n') {
 		terminal_column = 0;
 		terminal_row++;
-		return;
+	} else {
+		terminal_putentryat(c, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
+			terminal_row++;
+		}
 	}
 
-	terminal_putentryat(c, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
+
+	if (terminal_row == VGA_HEIGHT)
+		terminal_row = 0;
 }
 
 void terminal_print(const char* data)
