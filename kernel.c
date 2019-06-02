7788675812f8b5d9669d49d8bcae72ea1a760574
Ruben Ollesch <ruben.ollesch@pm.me>
2019-06-02 02:10:43 +0200

    Simplify terminal setcolor function

--- kernel.c
+++ kernel.c
@@ -32,11 +32,6 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
-{
-	return fg | bg << 4;
-}
-
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
 	return (uint16_t) uc | (uint16_t) color << 8;
@@ -58,11 +53,16 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
+void terminal_setcolor(enum vga_color fg, enum vga_color bg)
+{
+	terminal_color = fg | bg << 4;
+}
+
 void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_setcolor(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -70,11 +70,6 @@ void terminal_initialize(void)
 			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
 	}
-}
-
-void terminal_setcolor(uint8_t color)
-{
-	terminal_color = color;
 }
 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
