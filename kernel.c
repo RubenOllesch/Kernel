95f38bfa6139331a70b118a08c028deb661129cb
Ruben Ollesch <ruben.ollesch@pm.me>
2019-06-06 23:32:23 +0200

    Improve names of functions

--- kernel.c
+++ kernel.c
@@ -46,24 +46,24 @@ static const size_t VGA_HEIGHT = 25;
 static uint16_t* term_buff = (uint16_t*) 0xB8000;
 size_t term_row;
 size_t term_column;
-uint8_t terminal_color;
+uint8_t term_color;
 
 static inline size_t term_index(size_t x, size_t y)
 {
 	return y * VGA_WIDTH + x;
 }
 
-void terminal_setcolor(enum vga_color fg, enum vga_color bg)
+void term_setcolor(enum vga_color fg, enum vga_color bg)
 {
-	terminal_color = fg | bg << 4;
+	term_color = fg | bg << 4;
 }
 
-void terminal_putentryat(char c, size_t x, size_t y)
+void term_putentryat(char c, size_t x, size_t y)
 {
-	term_buff[term_index(x, y)] = c | terminal_color << 8;
+	term_buff[term_index(x, y)] = c | term_color << 8;
 }
 
-void terminal_scroll(void)
+void term_scroll(void)
 {
 	for (size_t row = 0; row < VGA_HEIGHT - 1; row++) {
 		for (size_t clmn = 0; clmn < VGA_WIDTH; clmn++) {
@@ -77,25 +77,25 @@ void terminal_scroll(void)
 	term_row--;
 }
 
-void terminal_initialize(void)
+void terminal_init(void)
 {
 	term_row = 0;
 	term_column = 0;
-	terminal_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
+	term_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			terminal_putentryat(0, x, y);
+			term_putentryat(0, x, y);
 		}
 	}
 }
 
-void terminal_putchar(char c)
+void term_putchar(char c)
 {
 	if (c == '\n') {
 		term_column = 0;
 		term_row++;
 	} else {
-		terminal_putentryat(c, term_column, term_row);
+		term_putentryat(c, term_column, term_row);
 		if (++term_column == VGA_WIDTH) {
 			term_column = 0;
 			term_row++;
@@ -104,20 +104,20 @@ void terminal_putchar(char c)
 
 
 	if (term_row == VGA_HEIGHT)
-		terminal_scroll();
+		term_scroll();
 }
 
-void terminal_print(const char* data)
+void print(const char* data)
 {
 	while(*data) {
-		terminal_putchar(*data++);
+		term_putchar(*data++);
 	}
 }
 
 void kernel_main(void)
 {
-	/* Initialize terminal interface */
-	terminal_initialize();
+	terminal_init();
 
-	terminal_print("Hello, kernel World!\n");
+
+	print("Hello, World\n");
 }
