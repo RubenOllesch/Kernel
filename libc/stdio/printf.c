386a89cce7c31281f41c8d05e3604cfda5f9eb67
Ruben Ollesch <ruben.ollesch@pm.me>
2019-06-26 15:37:48 +0200

    Add `%' support to printf

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -1,12 +1,39 @@
-#ifdef __is_libk
-#include <kernel/tty.h>
-#endif
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
+
 
 int printf(const char *format, ...)
 {
-#ifdef __is_libk
-	return (int) tty_print(format);
-#else
-	// TODO: Hosted libc implementation
-#endif
+	va_list parameters;
+	va_start(parameters, format);
+	
+	size_t written = 0;
+	char c;
+	bool isInFormat = false;
+
+	while ((c= *format++)) {
+		if (isInFormat) {
+			switch (c) {
+			case '%':
+				isInFormat = false;
+				putchar('%');
+				written++;
+				break;
+			}
+		} else {
+			if (c == '%') {
+				isInFormat = true;
+				continue;
+			}
+
+			putchar(c);
+			written++;
+		}
+	}
+
+  	va_end(parameters);
+
+	return written;
 }
