c72954cd63d63f5e4b5754e315014d94dde3cbc1
Ruben Ollesch <ruben.ollesch@pm.me>
2019-06-26 20:51:40 +0200

    Add abort to stdlib

---
+++ libc/stdlib/abort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifdef __is_libk
+#include <kernel/tty.h>
+#endif
+
+void abort(void)
+{
+#ifdef __is_libk
+	tty_setcolor(VGA_BLACK, VGA_RED);
+	printf("\nKernel: panic");
+	// TODO add core dump, etc..
+#else
+	// TODO user-space abort
+#endif
+}
