4bcb173e342362e44099f09cc6e95ee464dfede0
Ruben Ollesch <ruben.ollesch@pm.me>
2019-07-22 01:27:07 +0200

    Add sample source code to printed text

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,6 +4,19 @@
 
 #include "kernel/tty.h"
 
+const char *samplesource =
+	"size_t tty_print(const char *data)\n"
+	"{\n"
+	"\tsize_t len = 0;\n"
+	"\n"
+	"\twhile(data[len])\n"
+	"\t\ttty_putchar(data[len++]);\n"
+	"\n"
+	"\treturn len;\n"
+	"}\n"
+;
+
+
 void kernel_main(void)
 {
 	tty_init();
@@ -11,13 +24,18 @@ void kernel_main(void)
 
 	tty_setcolor(VGA_WHITE, VGA_BLACK);
 
-	puts("Hello, World!\n");
+	puts("Hello, World!");
 
-
-	for (size_t i = 0; i < 16; ++i) {
+	for (size_t i = 0; i < 8; ++i) {
 		tty_setcolor(i, VGA_BLACK);
-		printf("Colored Text on Black ");
+		printf("Colored Text on Black\t");
 		tty_setcolor(VGA_BLACK, i);
-		printf("Black Text on Colored Background\n");
+		printf("Black Text on Colored Background\t\n");
 	}
+
+	putchar('\n');
+
+	tty_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
+
+	puts(samplesource);
 }
